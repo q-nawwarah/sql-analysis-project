ba2f51e3588fa36dc1a3d5132d2c3fc17cb1de42
q-nawwarah <nawwarahyusof98@gmail.com>
2025-10-19 22:07:37 +0800

    Create 04_cumulative_cases.sql

---
+++ sql/04_cumulative_cases.sql
@@ -0,0 +1,11 @@
+-- 04_cumulative_cases.sql
+-- Purpose: Build cumulative trend to validate total vs daily
+-- 
+-- Author: Nawwarah
+-- Date: October 2025
+
+SELECT 
+  date,
+  SUM(new_cases) OVER (ORDER BY date) AS cumulative_cases
+FROM imperial-legacy-460710-q4.COVID19.owid_civid19
+WHERE location = 'Malaysia'
