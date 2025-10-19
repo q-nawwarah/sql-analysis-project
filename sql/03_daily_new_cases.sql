d5a430cf9e4c6cacdd80d8f2d724fa78c43b6a6d
q-nawwarah <nawwarahyusof98@gmail.com>
2025-10-19 21:47:05 +0800

    Update 03_daily_new_cases.sql

--- sql/03_daily_new_cases.sql
+++ sql/03_daily_new_cases.sql
@@ -1,1 +1,11 @@
+-- 03_daily_new_cases.sql
+-- Purpose: Extract daily new cases
+-- Author: Nawwarah
+-- Date: October 2025
 
+SELECT 
+  date,
+  new_cases AS daily_new_cases
+FROM imperial-legacy-460710-q4.COVID19.owid_civid19
+WHERE location = 'Malaysia'
+ORDER BY date;
